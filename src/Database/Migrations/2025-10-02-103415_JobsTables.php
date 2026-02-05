3f7983e0e1b9b5ede03e93ba5a9610b16530e180
Daycry <7590335+daycry@users.noreply.github.com>
2026-02-05 18:31:27 +0100

    Update migration namespace to Daycry\Jobs
    Change the namespace in JobsTables migration from Daycry\Cronjob\Database\Migrations to Daycry\Jobs\Database\Migrations to reflect the package/namespace rename. No functional changes to the migration logic.

--- src/Database/Migrations/2025-10-02-103415_JobsTables.php
+++ src/Database/Migrations/2025-10-02-103415_JobsTables.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * the LICENSE file that was distributed with this source code.
  */
 
-namespace Daycry\Cronjob\Database\Migrations;
+namespace Daycry\Jobs\Database\Migrations;
 
 use CodeIgniter\Database\Forge;
 use CodeIgniter\Database\Migration;
